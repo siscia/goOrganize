4c5d311966f4bad56e864e413a04a49b155cd675
Simone "siscia" Mosciatti <simone@mweb.biz>
2013-12-09 00:49:18 +0100

    some more works find out why of bug, add routes lib

--- goorganizer/api.go
+++ goorganizer/api.go
@@ -6,20 +6,32 @@ import (
 	"appengine"
 	"errors"
 	"encoding/json"
+	"hash/fnv"
+	"fmt"
 )
+
+func GenerateHash(text string, title string, moment time.Time) string{
+	h := fnv.New64a()
+	t := fmt.Sprintf("%x%x%x", text, title, moment)
+	h.Write([]byte(t))
+	return fmt.Sprintf("%x", int64(h.Sum64()))
+}
 
 
 func GetUser(c appengine.Context, email string) (User, error){
 	var user User 
 	key := datastore.NewKey(c, "Users", email, 0, nil) 
 	if datastore.Get(c, key, user) == datastore.ErrNoSuchEntity{
+		//panic("Not found email")
 		return CreateUser(email, c)}
+	c.Infof("user.Id: %v", user.Id)
 	return user, nil
 }
 
 func CreateUser(email string, c appengine.Context) (User, error) {
 	key := datastore.NewKey(c, "Users", email, 0, nil)
-	id := key.AppID()
+	id := key.StringID()
+	c.Infof("user create: %v", id)
 	user := User{Id: id, Email: email, Verified: false}
 	_, err := datastore.Put(c, key, &user)
 	if err != nil {
@@ -31,8 +43,10 @@ func NewThread(c appengine.Context, email string, title string, text string) (Th
 	author, erro := GetUser(c, email)
 	if erro != nil {
 		return Thread{}, erro}
-	key := datastore.NewIncompleteKey(c, "Thread", nil)
-	id := key.AppID()
+	hash := GenerateHash(text, title, time.Now())
+	key := datastore.NewKey(c, "Thread", hash, 0, nil)
+	id := key.StringID()
+	c.Infof("id of thread: %v", id)
 	thread := Thread{Id: id, Title: title, Text: text, Time: time.Now(), Participant: []string{author.Id}}
 	_, err := datastore.Put(c, key, &thread)
 	if err != nil {
@@ -48,20 +62,31 @@ func GetThread(c appengine.Context, id string) (Thread, error) {
 	return thread, nil
 }
 
+func GetPost(c appengine.Context, id string) (Post, error) {
+	key := datastore.NewKey(c, "Posts", id, 0, nil)
+	var post Post
+	if datastore.Get(c, key, post) == datastore.ErrNoSuchEntity{
+		return Post{}, datastore.ErrNoSuchEntity}
+	return post, nil
+}
+
 func NewPost(c appengine.Context, threadId string, email string, text string) (Thread, error){
 	thread, err := GetThread(c, threadId)
 	if err != nil {
 		return Thread{}, err}
 	user, err := GetUser(c, email)
+	c.Infof("thread.Id: %v user.Id: %v", thread.Id, user.Id)
 	for _, participant := range thread.Participant {
 		if participant == user.Id {
-			key := datastore.NewIncompleteKey(c, "Post", nil)
-			post := Post{Id: key.AppID(), Author: user.Id, Text: text, Time: time.Now()}
+			id := GenerateHash(email, text, time.Now())
+			key := datastore.NewKey(c, "Posts", id, 0, nil)
+			post := Post{Id: key.StringID(), Author: user.Id, Text: text, Time: time.Now()}
 			_, err := datastore.Put(c, key, post)
 			if err != nil{
 				panic("Error writing Post")}
 			return AddPost(c, thread, post)}}
-	return Thread{}, errors.New("Not Authenticate User")
+	panic("Non AUTH")
+	//return Thread{}, errors.New("Not Authenticate User")
 }
 
 func AddParticipant(c appengine.Context, thread Thread, user User) (Thread, User) {
@@ -78,34 +103,10 @@ func AddParticipant(c appengine.Context, thread Thread, user User) (Thread, User
 }
 
 func AddPost(c appengine.Context, thread Thread, post Post) (Thread, error){
-	new_posts := append(thread.Posts, post.Id)
-	thread.Posts = new_posts
+	thread.Posts =  append(thread.Posts, post.Id)
 	return UpdateThread(c, thread)
 }
 
-func UpdateUser(c appengine.Context, user User) (User, error){
-	key := datastore.NewKey(c, "User", user.Id, 0, nil)
-	_, err := datastore.Put(c, key, user)
-	if err != nil {
-		return User{}, err}
-	return user, nil
-}
-
-func UpdateThread(c appengine.Context, thread Thread) (Thread, error){
-	key := datastore.NewKey(c, "Thread", thread.Id, 0, nil)
-	_, err := datastore.Put(c, key, thread)
-	if err != nil{
-		return Thread{}, err}
-	return thread, nil
-}
-
-func UpdatePost(c appengine.Context, post Post) (Post, error){
-	key := datastore.NewKey(c, "Post", post.Id, 0, nil)
-	_, err := datastore.Put(c, key, post)
-	if err != nil {
-		return Post{}, err}
-	return post, nil
-}
 
 func DeletePost(c appengine.Context, thread Thread, index int) (Thread, error){
 	if len(thread.Posts) > index {
@@ -118,7 +119,7 @@ func DeletePost(c appengine.Context, thread Thread, index int) (Thread, error){
 func ModifyPost(c appengine.Context, thread Thread, index int, text string) (Post, error){
 	if len(thread.Posts) > index{
 		postID := thread.Posts[index]
-		key := datastore.NewKey(c, "Post", postID, 0, nil)
+		key := datastore.NewKey(c, "Posts", postID, 0, nil)
 		var post Post
 		if datastore.Get(c, key, post) == datastore.ErrNoSuchEntity{
 			return Post{}, datastore.ErrNoSuchEntity}
@@ -132,3 +133,26 @@ func JsonThread(thread Thread) ([]byte, error){
 	if err != nil {
 		return []byte{}, err}
 	return j, nil}
+
+func UpdateUser(c appengine.Context, user User) (User, error){
+	key := datastore.NewKey(c, "Users", user.Id, 0, nil)
+	_, err := datastore.Put(c, key, user)
+	if err != nil {
+		return User{}, err}
+	return user, nil
+}
+
+func UpdateThread(c appengine.Context, thread Thread) (Thread, error){
+	key := datastore.NewKey(c, "Threads", thread.Id, 0, nil)
+	_, err := datastore.Put(c, key, thread)
+	if err != nil{
+		return Thread{}, err}
+	return thread, nil
+}
+
+func UpdatePost(c appengine.Context, post Post) (Post, error){
+	key := datastore.NewKey(c, "Posts", post.Id, 0, nil)
+	_, err := datastore.Put(c, key, post)
+	if err != nil {
+		return Post{}, err}
+	return post, nil}
--- goorganizer/handler.go
+++ goorganizer/handler.go
@@ -5,20 +5,35 @@ import (
 	"net/http"
 	"time"
 	"appengine"
+	"github.com/gorilla/mux"
+//	"appengine/datastore"
 )
 
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/p", handler1)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	fmt.Fprint(w, time.Now())
-	NewThread(c, "simone@mweb.biz", "prova", "lala")
-	_, err := GetUser(c, "simone@mweb.biz")
-	fmt.Fprint(w, "\nHello, world!\n")
-	if err != nil{
-		fmt.Fprint(w, err)}
+	fmt.Fprint(w, time.Now(), "\n")
+	NewThread(c, "simone@mweb.biz", "prova", "la1la")
+	//if err != nil{fmt.Fprint(w, "???")}
+	//NewPost(c, thread.Id, "simone@mweb.biz", "ole")
+	//NewPost(c, thread.Id, "simone@mweb.biz", "sim")
+	//GetUser(c, "simone@mweb.biz")
+	//fmt.Fprint(w, thread.Participant)
 }
 
+
+func handler1(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	fmt.Fprint(w, time.Now(), "\n")
+	NewThread(c, "leonardo@mweb.biz", "noidea", "???")
+	//if err != nil{fmt.Fprint(w, "???")}
+	//NewPost(c, thread.Id, "simone@mweb.biz", "ole")
+	//NewPost(c, thread.Id, "simone@mweb.biz", "sim")
+	//GetUser(c, "simone@mweb.biz")
+	//fmt.Fprint(w, thread.Participant)
+}
